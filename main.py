151fa1cae6e6e2df4c4515c667eda1e02c4cf14d
LinkWaker <75544395+LinkWakr@users.noreply.github.com>
2025-12-04 18:47:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ with open("config.json", "r") as f:
 
 def find_command_name(trigger):
     for cmd in COMMANDS:
-        if (cmd["trigger"] == trigger):
-            return cmd
+        if (COMMANDS[cmd]["trigger"] == trigger):
+            return cmd    
 
 while(True):
     code = input("> ")
     splited_code = code.split()
     command = splited_code[0]
+    command_name = find_command_name(command)
+
     args = splited_code[1:]
-    if (COMMANDS[command]["min_args] <= len(args)):
-        print("Syntax Error. Usage: " + COMMANDS[command]["usage"])
+    
+    if (COMMANDS[command_name]["min_args"] > len(args)):
+        print("Syntax Error. Usage: " + COMMANDS[command_name]["usage"])
         continue
 
     if (command == COMMANDS["start_serial"]["trigger"]): # USAGE: -ss <PORT> <BOUNDRATE>
@@ -44,72 +47,61 @@ while(True):
                 message = receivedData.decode('utf-8').strip()
                 print(f"stm32> {message}")
                 receive = True
+    elif (command == COMMANDS["write"]["trigger"]):
+        ser.write(args[0].encode('utf-8'))
+        print("The message's writen successfully!")
+    elif (command == COMMANDS["read_save"]["trigger"]):
+        with open("data.json", "r") as f:
+            data_json = json.load(f)
 
-    elif (command == COMMANDS["write"]["trigger"]): #write USAGE: -w <message>
-        if (len(args) == 0):
-            print("Syntax Error. Usage: -w <message>!")
-        else:
-            ser.write(args[0].encode('utf-8'))
-            print("The message's writen successfully!")
-    elif (command == COMMANDS["read_save"]["trigger"]): #read save USAGE: -rs <seconds> <name>
-        if (len(args) == 0):
-            print("Syntax Error. Usage: -rs <seconds>!")
-        else:
-            data = []
-            starting_time = time.time()
+        data = []
+        starting_time = time.time()
+        while ((time.time() - starting_time) < int(args[0])):
+            receivedData = ser.readline()
+            data.append((time.time(), receivedData.decode('utf-8').strip()))
+        
+        id = data_json["saved_datas"][-1]["id"] + 1 if len(data_json["saved_datas"]) > 0 else 0
+        
+        data_object = {
+            "id": id,
+            "name": args[1],
+            "startingTime": starting_time,
+            "duration": int(args[0]),
+            "data": data
+        }
+        
+        data_json["saved_datas"].append(data_object)
 
-            while ((time.time() - starting_time) < int(args[0])):
-                receivedData = ser.readline()
-                data.append((time.time(), receivedData.decode('utf-8').strip()))
-            
-            id = data_json["savedDatas"][-1]["id"] + 1 if len(data_json["savedDatas"]) > 0 else 0
-            
-            data_object = {
-                "id": id,
-                "name": args[1],
-                "startingTime": starting_time,
-                "duration": int(args[0]),
-                "data": data
-            }
+        with open("data.json", "w") as f:
+            json.dump(data_json, f, indent=4)
 
-            with open("data.json", "r") as f:
-                data_json = json.load(f)
-            
-            data_json["savedDatas"].append(data_object)
-
-            with open("data.json", "w") as f:
-                json.dump(data_json, f, indent=4)
-
-            print(f"Read data has saved successfully! Save ID: {id}")
+        print(f"Read data has saved successfully! Save ID: {id}")
     
     elif (command == COMMANDS["plot"]["trigger"]): #plot USAGE: -p <save_ID>
-        if (len(args) > 0):
-            with open("data.json", "r") as f:
-                data_json = json.load(f)
+        with open("data.json", "r") as f:
+            data_json = json.load(f)
 
-            x_axis = []
-            y_axis = []
+        x_axis = []
+        y_axis = []
 
-            if (len(data_json["savedDatas"]) > int(args[0])):
-                datas = data_json["savedDatas"][int(args[0])]["data"]
-                name = data_json["savedDatas"][int(args[0])]["name"]
-                for data in datas:
-                    x_axis.append(data[0]) 
-                    y_axis.append(data[1])
+        if (len(data_json["saved_datas"]) > int(args[0])):
+            datas = data_json["saved_datas"][int(args[0])]["data"]
+            name = data_json["saved_datas"][int(args[0])]["name"]
+            for data in datas:
+                x_axis.append(data[0]) 
+                y_axis.append(data[1])
 
-                plt.figure(figsize=(10, 6))
+            plt.figure(figsize=(10, 6))
 
-                plt.plot(x_axis, y_axis, marker='o', linestyle='-', color='b', label=name)
+            plt.plot(x_axis, y_axis, marker='o', linestyle='-', color='b', label=name)
 
-                plt.title("Time - Data")
-                plt.xlabel("scnds")
-                plt.ylabel("data")
+            plt.title("Time - Data")
+            plt.xlabel("scnds")
+            plt.ylabel("data")
 
-                plt.grid(True) 
-                plt.legend()
-                plt.show()
-        else:
-            print("Syntax Error. Usage: -p <save_ID>!")
+            plt.grid(True) 
+            plt.legend()
+            plt.show()
 
     elif (command == COMMANDS["help"]["trigger"]):
         print("Help:")
